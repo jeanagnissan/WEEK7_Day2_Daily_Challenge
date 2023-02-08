c5650c3653b25f763c2977d8ac831fd6eb40ae0d
jeanagnissan <yoanefirst@gmail.com>
2023-02-08 18:38:56 +0000

    update app WEEK7_Day2_Daily_Challenge

---
+++ src/Book.java
@@ -0,0 +1,35 @@
+public class Book {
+    private Author author;
+    private String name;
+    private double price;
+    private int qty;
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " by " + author;
+    }
+}
